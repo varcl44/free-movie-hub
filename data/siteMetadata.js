6c8f65ec862b1fdcfcfb5cb3e337d2be396b920f
varcl44 <varcl44@gmail.com>
2025-08-14 14:43:15 +0800

    1

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Free Movie Blog',
   author: 'FreeMovieHub',
   headerTitle: 'FreeMovieHub',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog sharing free movie resources',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
